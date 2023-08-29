fc80b5f8c8bbe4e39fb4d4cbe92afff1b1f83364
PouPeuu <qwertydude277@gmail.com>
2023-08-29 14:10:15 +0300

    grog

---
+++ src/main/java/com/poupeuu/moderatelybadnid/blocks/RadioBlock.java
@@ -0,0 +1,16 @@
+package com.poupeuu.moderatelybadnid.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+public class RadioBlock extends BaseEntityBlock {
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos p_153215_, BlockState p_153216_) {
+        return null;
+    }
+}
