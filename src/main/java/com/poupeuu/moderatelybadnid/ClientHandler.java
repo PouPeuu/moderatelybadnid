30f8a6097961573e8749254d3817ff39982c5639
PouPeuu <qwertydude277@gmail.com>
2023-08-30 16:29:19 +0300

    Added mouse button down check

--- src/main/java/com/poupeuu/moderatelybadnid/ClientHandler.java
+++ src/main/java/com/poupeuu/moderatelybadnid/ClientHandler.java
@@ -2,11 +2,19 @@ package com.poupeuu.moderatelybadnid;
 
 import com.poupeuu.moderatelybadnid.registers.ModBlockEntities;
 import com.poupeuu.moderatelybadnid.renderers.ToasterBlockEntityRenderer;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import org.lwjgl.glfw.GLFW;
 
 public class ClientHandler {
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         BlockEntityRenderers.register(ModBlockEntities.TOASTER_BLOCK_ENTITY.get(), ToasterBlockEntityRenderer::new);
+    }
+
+    public static boolean isMouseButtonDown(int button) {
+        return GLFW.glfwGetMouseButton(Minecraft.getInstance()
+                .getWindow()
+                .getWindow(), button) == 1;
     }
 }
