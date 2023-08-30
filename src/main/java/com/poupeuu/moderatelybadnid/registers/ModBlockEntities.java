1e6dbe2e0d876431f933f42be5345805cb1bd7bf
PouPeuu <qwertydude277@gmail.com>
2023-08-30 16:28:48 +0300

    Fixed Radio BlockEntity being a toaster

--- src/main/java/com/poupeuu/moderatelybadnid/registers/ModBlockEntities.java
+++ src/main/java/com/poupeuu/moderatelybadnid/registers/ModBlockEntities.java
@@ -1,6 +1,7 @@
 package com.poupeuu.moderatelybadnid.registers;
 
 import com.poupeuu.moderatelybadnid.ModeratelyBadNid;
+import com.poupeuu.moderatelybadnid.blocks.blockEntities.RadioBlockEntity;
 import com.poupeuu.moderatelybadnid.blocks.blockEntities.ToasterBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,6 +12,6 @@ public class ModBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> REGISTER = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, ModeratelyBadNid.MODID);
 
     public static final RegistryObject<BlockEntityType<ToasterBlockEntity>> TOASTER_BLOCK_ENTITY = REGISTER.register("toaster", () -> BlockEntityType.Builder.of(ToasterBlockEntity::new, ModBlocks.TOASTER.get()).build(null));
-    public static final RegistryObject<BlockEntityType<ToasterBlockEntity>> RADIO_BLOCK_ENTITY = REGISTER.register("radio", () -> BlockEntityType.Builder.of(ToasterBlockEntity::new, ModBlocks.RADIO.get()).build(null));
+    public static final RegistryObject<BlockEntityType<RadioBlockEntity>> RADIO_BLOCK_ENTITY = REGISTER.register("radio", () -> BlockEntityType.Builder.of(RadioBlockEntity::new, ModBlocks.RADIO.get()).build(null));
 
 }
