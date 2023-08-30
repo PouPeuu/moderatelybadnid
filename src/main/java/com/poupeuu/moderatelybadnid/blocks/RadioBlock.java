9e4181f61409da5cc63253ec9de6f3ae89abc03e
PouPeuu <qwertydude277@gmail.com>
2023-08-30 16:45:30 +0300

    Added client tick to check if mouse button is down and not sneaking

--- src/main/java/com/poupeuu/moderatelybadnid/blocks/RadioBlock.java
+++ src/main/java/com/poupeuu/moderatelybadnid/blocks/RadioBlock.java
@@ -1,19 +1,24 @@
 package com.poupeuu.moderatelybadnid.blocks;
 
 import com.poupeuu.moderatelybadnid.blocks.blockEntities.RadioBlockEntity;
+import com.poupeuu.moderatelybadnid.registers.ModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nullable;
 
 public class RadioBlock extends BaseEntityBlock {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
@@ -23,6 +28,14 @@ public class RadioBlock extends BaseEntityBlock {
     public RadioBlock(Properties properties) {
         super(properties);
         this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Nullable
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> blockEntityType) {
+        if (level.isClientSide) {
+            return createTickerHelper(blockEntityType, ModBlockEntities.RADIO_BLOCK_ENTITY.get(), RadioBlockEntity::clientTick);
+        }
+        return null;
     }
 
     @Override
--- src/main/java/com/poupeuu/moderatelybadnid/blocks/blockEntities/RadioBlockEntity.java
+++ src/main/java/com/poupeuu/moderatelybadnid/blocks/blockEntities/RadioBlockEntity.java
@@ -1,7 +1,12 @@
 package com.poupeuu.moderatelybadnid.blocks.blockEntities;
 
+import com.poupeuu.moderatelybadnid.ClientHandler;
+import com.poupeuu.moderatelybadnid.ModeratelyBadNid;
+import com.poupeuu.moderatelybadnid.blocks.RadioBlock;
 import com.poupeuu.moderatelybadnid.registers.ModBlockEntities;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
@@ -9,5 +14,11 @@ import net.minecraft.world.level.block.state.BlockState;
 public class RadioBlockEntity extends BlockEntity {
     public RadioBlockEntity(BlockPos blockPos, BlockState blockState) {
         super(ModBlockEntities.RADIO_BLOCK_ENTITY.get(), blockPos, blockState);
+    }
+
+    public static void clientTick(Level level, BlockPos blockPos, BlockState blockState, RadioBlockEntity radioBlockEntity){
+        if(ClientHandler.isMouseButtonDown(1) && !Minecraft.getInstance().player.isShiftKeyDown()){
+            ModeratelyBadNid.sendChatMessage("Right Mouse Down and not shifting");
+        }
     }
 }
