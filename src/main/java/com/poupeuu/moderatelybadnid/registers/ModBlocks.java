b3e54738ec6b3d8ab2e1b2bb922a4ab9207be9bc
PouPeuu <qwertydude277@gmail.com>
2023-08-30 15:22:35 +0300

    Fixed radio being a toaster

--- src/main/java/com/poupeuu/moderatelybadnid/registers/ModBlocks.java
+++ src/main/java/com/poupeuu/moderatelybadnid/registers/ModBlocks.java
@@ -1,6 +1,7 @@
 package com.poupeuu.moderatelybadnid.registers;
 
 import com.poupeuu.moderatelybadnid.ModeratelyBadNid;
+import com.poupeuu.moderatelybadnid.blocks.RadioBlock;
 import com.poupeuu.moderatelybadnid.blocks.RedBlock;
 import com.poupeuu.moderatelybadnid.blocks.ToasterBlock;
 import net.minecraft.world.level.block.Block;
@@ -14,5 +15,5 @@ public class ModBlocks {
     public static final DeferredRegister<Block> REGISTER = DeferredRegister.create(ForgeRegistries.BLOCKS, ModeratelyBadNid.MODID);
     public static final RegistryObject<Block> RED_BLOCK = REGISTER.register("red_block", () -> new RedBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_RED)));
     public static final RegistryObject<Block> TOASTER = REGISTER.register("toaster", () -> new ToasterBlock(BlockBehaviour.Properties.of().noOcclusion()));
-    public static final RegistryObject<Block> RADIO = REGISTER.register("radio", () -> new ToasterBlock(BlockBehaviour.Properties.of().noOcclusion()));
+    public static final RegistryObject<Block> RADIO = REGISTER.register("radio", () -> new RadioBlock(BlockBehaviour.Properties.of().noOcclusion()));
 }
