bfce7350ff4b9fcb87dc68ad8e797584effd3caf
PouPeuu <qwertydude277@gmail.com>
2023-08-30 15:35:56 +0300

    Added the radio block

--- src/main/java/com/poupeuu/moderatelybadnid/blocks/RadioBlock.java
+++ src/main/java/com/poupeuu/moderatelybadnid/blocks/RadioBlock.java
@@ -2,18 +2,62 @@ package com.poupeuu.moderatelybadnid.blocks;
 
 import com.poupeuu.moderatelybadnid.blocks.blockEntities.RadioBlockEntity;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class RadioBlock extends BaseEntityBlock {
-    protected RadioBlock(Properties properties) {
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    protected static final VoxelShape SHAPE_X = Block.box(1, 0, 5, 15, 10, 11);
+    protected static final VoxelShape SHAPE_Z = Block.box(5,0,1, 11, 10,15);
+
+    public RadioBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new RadioBlockEntity(blockPos, blockState);
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+        return this.defaultBlockState().setValue(FACING, blockPlaceContext.getHorizontalDirection().getOpposite());
+    }
+
+    public BlockState rotate(BlockState blockState, Rotation rotation) {
+        return blockState.setValue(FACING, rotation.rotate(blockState.getValue(FACING)));
+    }
+
+    public BlockState mirror(BlockState blockState, Mirror mirror) {
+        return blockState.rotate(mirror.getRotation(blockState.getValue(FACING)));
+    }
+
+    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext){
+        //return SHAPE;
+        switch ((Direction)blockState.getValue(FACING)) {
+            case NORTH, SOUTH:
+                return SHAPE_X;
+            default:
+                return SHAPE_Z;
+        }
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
     }
 }
--- src/main/java/com/poupeuu/moderatelybadnid/blocks/blockEntities/RadioBlockEntity.java
+++ src/main/java/com/poupeuu/moderatelybadnid/blocks/blockEntities/RadioBlockEntity.java
@@ -7,7 +7,7 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class RadioBlockEntity extends BlockEntity {
-    public RadioBlockEntity(BlockPos p_155229_, BlockState p_155230_) {
-        super(ModBlockEntities.RADIO_BLOCK_ENTITY.get(), p_155229_, p_155230_);
+    public RadioBlockEntity(BlockPos blockPos, BlockState blockState) {
+        super(ModBlockEntities.RADIO_BLOCK_ENTITY.get(), blockPos, blockState);
     }
 }
